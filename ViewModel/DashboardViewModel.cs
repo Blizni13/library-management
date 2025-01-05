88d42fb6210b8de7879543547886d31088c06367
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-05 11:42:46 +0100

    dashboard with basic structure created

---
+++ ViewModel/DashboardViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace library_management.ViewModel
+{
+    class DashboardViewModel
+    {
+        public ICommand ExitCommand { get; }
+
+        public DashboardViewModel()
+        {
+            ExitCommand = new RelayCommand(Exit);
+        }
+
+        public void Exit()
+        {
+            MessageBoxResult result
+                = MessageBox.Show("Are you sure you want to exit?", "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+                Application.Current.Shutdown();
+            
+        }
+    }
+}
