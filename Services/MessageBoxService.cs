c46fa8e3e4fd753d409194209fe8ace9ea62b633
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-05 18:48:16 +0100

    simple methods to create message boxes faster

---
+++ Services/MessageBoxService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace library_management.Services
+{
+    public class MessageBoxService
+    {
+        public static void ShowSuccessBox(string message, string? title = null)
+        {
+            MessageBox.Show(message, title ?? "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        
+        public static void ShowErrorBox(string message, string? title = null)
+        {
+            MessageBox.Show(message, title ?? "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        
+        public static void ShowWarningBox(string message, string? title = null)
+        {
+            MessageBox.Show(message, title ?? "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+    }
+}
