c3092d581e6208d374896d3b4b0ee18a76f75904
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-05 18:51:19 +0100

    open add book changes, close add book added

--- ViewModel/DashboardViewModel.cs
+++ ViewModel/DashboardViewModel.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using library_management.Services;
+using library_management.View;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,11 +14,18 @@ namespace library_management.ViewModel
 {
     class DashboardViewModel
     {
+        // windows
+        private AddBook? _addBookWindow;
+
+        // commands
         public ICommand ExitCommand { get; }
+        public ICommand OpenAddBookCommand { get; }
 
         public DashboardViewModel()
         {
+            _addBookWindow = null;
             ExitCommand = new RelayCommand(Exit);
+            OpenAddBookCommand = new RelayCommand(OpenAddBook);
         }
 
         public void Exit()
@@ -25,6 +35,27 @@ namespace library_management.ViewModel
             if (result == MessageBoxResult.Yes)
                 Application.Current.Shutdown();
             
+        }
+
+        public void OpenAddBook()
+        {
+            if (_addBookWindow == null)
+            {
+                _addBookWindow = new AddBook()
+                {
+                    DataContext = new AddBookViewModel(this)
+                };
+                _addBookWindow.Show();
+            }
+            else
+            {
+                _addBookWindow.Activate();
+            }
+        }
+
+        public void CloseAddBook()
+        {
+            _addBookWindow?.Close();
         }
     }
 }
