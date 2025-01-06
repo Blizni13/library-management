5e11df3c6728cd7d14c0fa7e26f99e772b632f1d
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-06 12:50:15 +0100

    book model added in order to use it when updating a book

---
+++ Model/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_management.Model
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publication { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Book(int id, string title, string author, string publication, decimal price, int quantity)
+        {
+            ID = id;
+            Title = title;
+            Author = author;
+            Publication = publication;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+
+}
