60ad1ba6e82bdc854c362e3238ce5aae676e24a2
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-06 22:03:52 +0100

    add reader window added

---
+++ View/AddReader.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace library_management.View
+{
+    /// <summary>
+    /// Interaction logic for AddReader.xaml
+    /// </summary>
+    public partial class AddReader : Window
+    {
+        public AddReader()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Start dragging the window
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
