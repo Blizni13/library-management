426c00b5335304ea68f8d416af5fd2f60754e6bb
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-06 22:03:39 +0100

    Window service added

---
+++ Services/WindowService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace library_management.Services
+{
+    public class WindowService
+    {
+        public static bool IsWindowOpen<T>() where T: Window
+        {
+            return Application.Current.Windows.OfType<T>().Any();
+        }
+    }
+}
