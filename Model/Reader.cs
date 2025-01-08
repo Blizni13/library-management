c65d1c776ee8e3f839ab78e3a695217e45f22ce4
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-08 20:47:34 +0100

    Reader model created

---
+++ Model/Reader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_management.Model
+{
+    public class Reader
+    {
+        public int ReaderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public DateTime MembershipStartDate { get; set; }
+        public Reader(
+            int readerId, 
+            string firstName, 
+            string lastName, 
+            DateTime dateOfBirth, 
+            string gender, 
+            string address, 
+            DateTime membershipStartDate
+        )
+        {
+            ReaderId = readerId;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            Address = address;
+            MembershipStartDate = membershipStartDate;
+        }
+    }
+}
