2dcfd8682cb0e37d41b534bee3dbc8ca012ad884
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-18 12:20:41 +0100

    IssuedBook model added

---
+++ Model/IssuedBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_management.Model
+{
+    class IssuedBook
+    {
+        public int ID { get; set; }
+        public int ReaderId { get; set; }
+        public string BookInfo { get; set; }
+        public DateTime IssueDate { get; set; }
+        public IssuedBook(int id, int readerId, string bookInfo, DateTime issueDate)
+        {
+            ID = id;
+            ReaderId = readerId;
+            BookInfo = bookInfo;
+            IssueDate = issueDate;
+        }
+    }
+}
