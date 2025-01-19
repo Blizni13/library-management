e4f1d9e06e3435ce2963e5acb7ddf3b9175ea356
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-19 09:44:15 +0100

    returned book model created

---
+++ Model/ReturnedBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_management.Model
+{
+    class ReturnedBook
+    {
+        public int ID { get; set; }
+        public int ReaderId { get; set; }
+        public string BookInfo { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public ReturnedBook(int id, int readerId, string bookInfo, DateTime issueDate, DateTime returnDate)
+        {
+            ID = id;
+            ReaderId = readerId;
+            BookInfo = bookInfo;
+            IssueDate = issueDate;
+            ReturnDate = returnDate;
+        }
+    }
+}
